bfc13f14fc6ff835d7e78d89f3724257ecfa18a4
s2019w33@e-mirim.hs.kr <s2019w33@e-mirim.hs.kr>
2021-02-26 14:59:10 +0900

    #2. mongoDB connect!

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://julianne:qawsedrftg@mongodbtest.0rhjr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.log(err))
+
 app.get('/', (req, res) => res.send('Hello World! 하이하이!'))
 // 앱이 실행되면 => 해라
 app.listen(port, () => console.log(`Example app listening on port ${port}`))
