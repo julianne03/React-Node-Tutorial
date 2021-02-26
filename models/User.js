04680c907ca8ffea548ac72af5a0a6ab89d4f2a7
s2019w33@e-mirim.hs.kr <s2019w33@e-mirim.hs.kr>
2021-02-26 15:06:35 +0900

    #3. MongoDB Model & Schema

---
+++ models/User.js
@@ -0,0 +1,34 @@
+// mongoose 가져오기
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        maxlength: 50
+    },
+    email: {
+        type: String,
+        trim: true,
+        unique: 1
+    },
+    password: {
+        type: String,
+        maxlength: 50
+    },
+    role: {
+        type: Number,
+        default: 0
+    },
+    image: String,
+    token: {
+        type: String
+    },
+    tokenExp: {
+        type: Number
+    }
+})
+
+const User = mongoose.model('User', userSchema);
+
+// 다른 곳에서도 사용할 수 있게
+module.exports = {User}
