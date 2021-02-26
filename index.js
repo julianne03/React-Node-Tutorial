c285c515785440221e3e445ed4e786388efef358
s2019w33@e-mirim.hs.kr <s2019w33@e-mirim.hs.kr>
2021-02-26 17:19:57 +0900

    #4. BodyParser & PostMan & 회원가입 기능

--- index.js
+++ index.js
@@ -2,6 +2,14 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser')
+const { User } = require("./models/User")
+
+//application/x-www-form-urlendcoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://julianne:qawsedrftg@mongodbtest.0rhjr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
@@ -10,5 +18,20 @@ mongoose.connect('mongodb+srv://julianne:qawsedrftg@mongodbtest.0rhjr.mongodb.ne
     .catch(err => console.log(err))
 
 app.get('/', (req, res) => res.send('Hello World! 하이하이!'))
+
+// register router
+app.post('/register', (req, res) => {
+    // 회원가입 시 필요한 정보들을 client에서 가져오면 
+    // 그 정보를 데이터베이스에 넣어주는 함수
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if(err) return res.json({sucess: false, err})
+        return res.status(200).json({
+            sucess: true
+        })
+    })
+})
+
 // 앱이 실행되면 => 해라
 app.listen(port, () => console.log(`Example app listening on port ${port}`))
