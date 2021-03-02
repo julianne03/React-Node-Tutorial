d776d9691e5a56164cc23667c6a95d31f60c1bcd
s2019w33@e-mirim.hs.kr <s2019w33@e-mirim.hs.kr>
2021-03-02 21:32:31 +0900

    #21. logout

--- server/index.js
+++ server/index.js
@@ -90,9 +90,9 @@ app.get('/api/users/logout', auth, (req, res) => {
     User.findOneAndUpdate({ _id: req.user._id },
         { token: "" }
         , (err, user) => {
-            if(err) return res.json({ sucess:false, err });
+            if(err) return res.json({ success:false, err });
             return res.status(200).send({
-                suceess: true
+                success: true
             })
         })
 })
